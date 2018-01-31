82875852da11468e0dd4040b164f0bc8659f423d
Luffy <devin.he@autodesk.com>
2018-01-31 09:12:37 +0800

    Update web pages

--- skypool/frontend/views.py
+++ skypool/frontend/views.py
@@ -18,8 +18,8 @@ class NewsIndexView(View):
     def get(self, request, *args, **kwargs):
         ctx = {}
         ctx['q'] = q = request.GET.get('q', '')
-        ctx['brand_id'] = brand_id = request.GET.get('brand_id', '')
-        ctx['intent'] = intent = request.GET.get('intent', '')
+        ctx['brand_id'] = brand_id = request.GET.get('brand_ids', '')
+        ctx['intent'] = intent = request.GET.get('intent_ids', '')
         ctx['brands'] = Brand.objects.all()
         ctx['intents'] = Activity.objects.all().values('intent').distinct()
 
@@ -32,13 +32,23 @@ class NewsIndexView(View):
         activities = paginator.get_page(page)
         ctx['activities'] = activities
         ctx['paginator'] = paginator
+        ctx['page_name'] = 'news-index'
         return render(request, self.template_name, ctx)
 
 class NewsShowView(View):
     template_name = "frontend/news/show.html"
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request, id):
         ctx = {}
+        activity = Activity.objects.filter(id=id).first()
+        ctx["activity"] = activity
+        ctx["brand"] = activity.brand
+        articles = Article.objects.all()[:5]
+        ctx["articles"] = articles
+        brand = activity.brand
+        ctx["brand"] = {}
+        ctx["brand"]["name"] = brand.name
+        ctx["brand"]["figure"] = [brand.total_popularity_score,brand.total_figure_score,brand.total_market_score,brand.total_innovation_score,brand.total_capital_score,brand.total_market_score]
         return render(request, self.template_name, ctx)
 
 class ArticlesShowView(View):
@@ -51,8 +61,25 @@ class ArticlesShowView(View):
 class BrandShowView(View):
     template_name = "frontend/brand/show.html"
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request, id):
         ctx = {}
+        brand = Brand.objects.filter(id=id).first()
+        ctx["brand_obj"] = brand
+        articles = Article.objects.all()[:5]
+        ctx["articles"] = articles
+        ctx["brand"] = {}
+        ctx["brand"]["name"] = brand.name
+        ctx["brand"]["figure"] = [brand.total_popularity_score,brand.total_figure_score,brand.total_market_score,brand.total_innovation_score,brand.total_capital_score,brand.total_market_score]
+        activities = Activity.objects.all().order_by('create_time')
+        paginator = Paginator(activities, 2)
+        page = request.GET.get('page', 0)
+        if page == 0:
+            page = 1
+        ctx['current_page'] = int(page)
+        activities = paginator.get_page(page)
+        ctx['activities'] = activities
+        ctx['paginator'] = paginator
+        ctx['page_name'] = 'brand-show'
         return render(request, self.template_name, ctx)
 
 
