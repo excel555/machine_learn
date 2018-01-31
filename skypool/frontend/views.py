8353d8283872621060c95dc7ae00aaf958e61f05
Luffy <devin.he@autodesk.com>
2018-01-31 11:29:33 +0800

    Update

--- skypool/frontend/views.py
+++ skypool/frontend/views.py
@@ -18,18 +18,20 @@ class NewsIndexView(View):
     def get(self, request, *args, **kwargs):
         ctx = {}
         ctx['q'] = q = request.GET.get('q', '')
-        ctx['brand_id'] = brand_id = request.GET.getlist('brand_id', '')
-        ctx['intent'] = intent = request.GET.getlist('intent_name', '')
+        brand_id = request.GET.getlist('brand_id', '')
+        intent = request.GET.getlist('intent_name', '')
         ctx['brands'] = Brand.objects.all()
         ctx['intents'] = Activity.objects.exclude(intent='None').all().values('intent').distinct()
 
         activities = Activity.objects.all()
 
-        if brand_id:
-            activities = activities.filter(brand__id__in=[_ for _ in brand_id if _ != 'all'])
+        brand_id = [_ for _ in brand_id if _ != 'all']
+        if brand_id and len(brand_id) > 0:
+            activities = activities.filter(brand__id__in=brand_id)
 
-        if intent:
-            activities = activities.filter(intent__in=[_ for _ in intent if _ != 'all'])
+        intent = [_ for _ in intent if _ != 'all']
+        if intent and len(intent) > 0:
+            activities = activities.filter(intent__in=intent)
 
         if q:
             activities = activities.filter(brand__name__contains=q)
@@ -44,6 +46,9 @@ class NewsIndexView(View):
         ctx['activities'] = activities
         ctx['paginator'] = paginator
         ctx['page_name'] = 'news-index'
+        ctx['brand_id'] = brand_id
+        ctx['intent_names'] = intent
+
         return render(request, self.template_name, ctx)
 
 class NewsShowView(View):
@@ -54,8 +59,8 @@ class NewsShowView(View):
         activity = Activity.objects.filter(id=id).first()
         ctx["activity"] = activity
         ctx["brand"] = activity.brand
-        articles = Article.objects.all()[:5]
-        ctx["articles"] = articles
+        ctx["articles"] = activity.articles()[:5]
+
         brand = activity.brand
         ctx["brand"] = {}
         ctx["brand"]["name"] = brand.name
@@ -85,12 +90,14 @@ class BrandShowView(View):
         ctx = {}
         brand = Brand.objects.filter(id=id).first()
         ctx["brand_obj"] = brand
+
         articles = Article.objects.all()[:5]
         ctx["articles"] = articles
         ctx["brand"] = {}
         ctx["brand"]["name"] = brand.name
         ctx["brand"]["figure"] = [brand.total_popularity_score,brand.total_figure_score,brand.total_market_score,brand.total_innovation_score,brand.total_capital_score]
         activities = Activity.objects.all().order_by('create_time')
+
         paginator = Paginator(activities, 15)
         page = request.GET.get('page', 0)
         if page == 0:
